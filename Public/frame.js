2271dac49a37cd0ce6b97840487fc498ee960c46
Ben D <benjamin.d928@icloud.com>
2019-08-09 18:10:14 +0100

    User can roll a strike and spare

--- Public/frame.js
+++ Public/frame.js
@@ -8,6 +8,10 @@ function Frame() {
   this.isSpare = false;
 };
 
+Frame.prototype.getScore = function() {
+  return this.frameScore;
+}
+
 Frame.prototype.totalFrame = function() {
   this.frameScore = this.score1 + this.score2;
   return this.frameScore;
@@ -15,13 +19,16 @@ Frame.prototype.totalFrame = function() {
 
 Frame.prototype.roll1 = function(roll) {
   if (roll == 10) {
+    this.totalFrame()
     return this.isStrike = true;
   }
   return this.score1 += roll;
 }
 
 Frame.prototype.roll2 = function(roll) {
-  if (this.roll1 + roll == 10) {
+  this.score2 = roll
+  this.totalFrame()
+  if (this.frameScore == 10) {
     return this.isSpare = true;
   }
   return this.score2 += roll;
--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -12,20 +12,24 @@ describe("Frame", function() {
     it('scores a frame of 8 and 1 = 9', function() {
       frame.roll1(8)
       frame.roll2(1)
-      frame.totalFrame()
       expect(frame.frameScore).toEqual(9);
     })
-    //   it('remaining pins will be 8 after a two roll', function() {
-    //     frame.roll(2)
-    //     expect(frame.remainingPins).toEqual(8);
-    //   })
-    // })
-    //
-    // describe('when player rolls a strike', function() {
-    //   it('a player rolls strike on first roll', function() {
-    //     frame.roll(10)
-    //     expect(frame.isStrike).toBe(true);
-    //   })
-    // })
-  })
+  });
+
+  describe('user rolls a strike', function() {
+    it('user rolls a strike', function() {
+      frame.roll1(10)
+      expect(frame.isStrike).toEqual(true);
+    })
+  });
+
+  describe('when player rolls a spare', function() {
+    it('a player rolls spare with two rolls', function() {
+      frame.roll1(8)
+      frame.roll2(2)
+      expect(frame.isSpare).toBe(true);
+      expect(frame.getScore()).toEqual(10);
+    })
+  });
+
 });
